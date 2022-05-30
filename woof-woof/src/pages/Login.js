aa77d500a168df1688bd7f4c091c95bf52b204b6
surendirabalaji <86005218+surendirabalaji@users.noreply.github.com>
2022-05-30 21:06:38 +0530

    changes

--- woof-woof/src/pages/Login.js
+++ woof-woof/src/pages/Login.js
@@ -1,19 +1,30 @@
 import Card from "../components/ui/Card";
 import classes from './Login.module.css'
+import React, {useState} from 'react';
 
 function LoginPage() {
+  const [email,setEmail]=useState(null);
+  const [password,setPassword]=useState(null)
+
+  function submitHandler(event){
+    console.log('hello');
+    console.log(email);
+    console.log(password);
+    event.preventDefault();
+  }
+
     return (
       <div className={classes.wrapper}>
         <Card>
-        <form action="login.php" method="POST" className={classes.form}>
+        <form method="POST" className={classes.form} onSubmit={submitHandler}>
           <div >
               <div className={classes.control}>
-                <label for="email" >Email</label>
-                <input type="email" id="email" name="email" placeholder="Enter email ID" required/>                 
+                <label htmlFor="email" >Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter email ID" required onChange={(e)=> setEmail(e.target.value)}/>                 
               </div>
               <div className={classes.control}>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password"  placeholder="Enter password" required/>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password"  placeholder="Enter password" required onChange={(e) => setPassword(e.target.value)}/>
               </div>
               <div className={classes.actions}>
                 <button type="submit" name="submit" id="submit">LOGIN</button>          
--- woof-woof/src/pages/signup.js
+++ woof-woof/src/pages/signup.js
@@ -1,36 +1,55 @@
 import Card from "../components/ui/Card";
 import classes from './Signup.module.css'
+import {useState} from 'react';
+
 function SignUp(){
-    return(
+        const [Name, setName] = useState(null);
+        const [Email, setEmail] = useState(null);
+        const [Password, setPassword] = useState(null);
+        const [Phone, setPhone] = useState(null);
+        const [UserType, setUserType] = useState(null);
+        const [Address, setAddress] = useState(null);
+
+        function submitHandler(event){
+                console.log(Name);
+                console.log(Email);
+                console.log(Password);
+                console.log(Phone);
+                console.log(UserType);
+                console.log(Address);
+                event.preventDefault();
+        }
+        return(
         <div className={classes.wrapper}>
         <Card>
-        <form action="signup.php" method="POST" className={classes.form}>
+        <form onSubmit={submitHandler} method="POST" className={classes.form}>
         <div className={classes.control}>    
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" size="30" maxlength="30" placeholder="Enter your name" required/>
+                <input type="text" name="name" id="name" size="30" maxLength={30} placeholder="Enter your name" required onChange={(e)=> setName(e.target.value)}/>
+        </div>
+
+        <div className={classes.control}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter a valid email ID" size="30" required onChange={(e)=> setEmail(e.target.value)}/>
         </div>
         <div className={classes.control}>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" maxlength="30" size="30" placeholder="Enter password" required/>
-        </div>
-        <div className={classes.control}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Enter a valid email ID" size="30" required/>
+                <input type="password" id="password" name="password" maxLength={30} size="30" placeholder="Enter password" required onChange={(e)=> setPassword(e.target.value)}/>
         </div>
         <div  className={classes.control}>    
                 <label htmlFor="phone">Phone</label>
-                <input type="number" id="phone" name="phone" placeholder="Enter a valid Phone number" size="12" required/>
+                <input type="number" id="phone" name="phone" placeholder="Enter a valid Phone number" size="12" required onChange={(e)=> setPhone(e.target.value)}/>
         </div>       
         <div className={classes.control}>
-                <label htmlFor="user">User Type</label>
-                <select id="user" name="user">
+                <label htmlFor="user" >User Type</label>
+                <select id="user" name="user" value={UserType} onChange={(e)=> setUserType(e.target.value)}>
                 <option value="Individual">Individual</option>
                 <option value="Organisation">Organisation</option>
                 </select>
         </div>
         <div className={classes.control}>
                 <label htmlFor="address">Address</label>
-                <input type="text" id="address" name="address" placeholder="Enter Address" size="25" required/>
+                <input type="text" id="address" name="address" placeholder="Enter Address" size="25" required onChange={(e)=> setAddress(e.target.value)}/>
         </div>
         <div className={classes.actions}>
                 <button type="submit" id="submit" name="submit">SIGN UP</button>
