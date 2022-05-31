351683bd9f177de0d6124436848f3f6e6d83adca
Pavan Adithya <pavanadithyak.s@gmail.com>
2022-05-31 11:48:49 +0530

    removed comments

--- server/app.js
+++ server/app.js
@@ -22,29 +22,6 @@ const pool = mysql.createPool({
     database : 'WoofDB'
 })
 
-/*app.post('/:id', (req,res) => {
-    console.log(req.params)
-   
-    pool.getConnection((err,connection) => {
-        if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
-        connection.query('SELECT * FROM users WHERE u_id = ?',[req.params.id], (err,rows) => {
-            connection.release() 
-
-            if(!err)
-            {
-                res.send(rows)
-           
-            }
-            else
-            {
-                console.log(err)
-            } 
-        })
-    })
-
-})
-*/
 
 app.post('/signup', (req,res) => {
     const input = req.body;
