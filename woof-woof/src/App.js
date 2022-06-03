115c915abec4b63d79d3bb5169dc0dde27181788
surendirabalaji <86005218+surendirabalaji@users.noreply.github.com>
2022-06-03 19:06:28 +0530

    changes

--- woof-woof/src/App.js
+++ woof-woof/src/App.js
@@ -2,13 +2,15 @@ import React, {useState} from 'react'
 import AppDashboard from './AppDashboardNavigation'
 import AppMainNavigation from './AppMainNavigation'
 
+
 function App() {
     const [login, setlogin] = useState(false)
+
   return (
     
     <div>
         {!login  && <AppMainNavigation setRender={setlogin}/>} 
-        {login && <AppDashboard/>}  
+        {login && <AppDashboard/>} 
     </div>
   )
 }
---
+++ woof-woof/src/pages/Upload/Geolocation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Geolocation(){
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+}
+
+export default Geolocation;
