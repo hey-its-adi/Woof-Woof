81e358a8cadb652eb08f3d2ecbb0debc3cf5d376
Pavan Adithya <pavanadithyak.s@gmail.com>
2022-06-04 21:24:51 +0530

    phpmyad

--- WoofDB.sql
+++ WoofDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 31, 2022 at 07:53 AM
+-- Generation Time: Jun 04, 2022 at 05:54 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -20,6 +20,38 @@ SET time_zone = "+00:00";
 --
 -- Database: `WoofDB`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `animals`
+--
+
+CREATE TABLE `animals` (
+  `a_id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `phone` int(12) NOT NULL,
+  `vaccine` varchar(30) NOT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `img` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `animals`
+--
+
+INSERT INTO `animals` (`a_id`, `name`, `location`, `phone`, `vaccine`, `fname`, `img`) VALUES
+(4, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(5, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(6, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(7, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(8, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(9, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(10, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(11, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(12, 'jcbkjs', 'vver', 23424, 'Vaccinated', NULL, NULL),
+(13, 'sgg', 'vver', 23424, 'Vaccinated', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -42,13 +74,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_id`, `name`, `email`, `phone`, `password`, `address`, `type`) VALUES
-(1, 'Ashwin', 'ashwinr@gmail.com', 39482982, 'aghast', 'chennai', 'organisation'),
-(2, 'Pavan Adithya', 'pavanadithyak.s@gmail.com', 231782238, 'asdf', 'chennai', 'individual'),
-(3, 'pavan', 'pavanadithya@kjd.com', 398948698, 'skeihuskj', 'chennai', 'Organisation');
+(1, 'Ashwin', 'ashwinr@gmail.com', 39482982, 'asdf', 'chennai', 'organisation'),
+(5, 'Pavan', 'pavan@gmail.com', 2898298, 'asdf', 'chennai', 'null');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `animals`
+--
+ALTER TABLE `animals`
+  ADD PRIMARY KEY (`a_id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +98,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `animals`
+--
+ALTER TABLE `animals`
+  MODIFY `a_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
