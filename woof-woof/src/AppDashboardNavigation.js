948116b80f2b7dfa6c318b496acda240fb6a1cfb
surendirabalaji <86005218+surendirabalaji@users.noreply.github.com>
2022-06-05 21:01:09 +0530

    changes

--- woof-woof/src/AppDashboardNavigation.js
+++ woof-woof/src/AppDashboardNavigation.js
@@ -4,6 +4,7 @@ import FeedPage from './pages/Feed/FeedPage'
 import DashboardNavigation from './components/layout/DashboardNavigation';
 import classes from "./App.module.css"
 import UploadPage from './pages/Upload/UploadPage';
+import ProfilePage from './pages/Profile/ProfilePage';
 
 function AppDashboard({forwardUsername}) {
 return(
@@ -12,7 +13,7 @@ return(
         <DashboardNavigation/> 
         <Routes>
           <Route path='/Upload' element={<UploadPage forwardUsername={forwardUsername}/>}/>
-          <Route path='/Profile'/>
+          <Route path='/Profile' element={<ProfilePage forwardUsername={forwardUsername}/>}/>
           <Route path='/Achievments'/>
           <Route path='/Dashboard' element={<FeedPage forwardUsername={forwardUsername}/>}/>
           <Route path='*' element={<FeedPage forwardUsername={forwardUsername}/>}/>
---
+++ woof-woof/src/pages/Profile/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePage  ({forwardUsename})  {
+  return (
+    <div>
+        ProfilePage
+    </div>
+  )
+}
+
+export default ProfilePage
