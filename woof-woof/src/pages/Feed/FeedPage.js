6710cb81e0b4e1414a6396423e14fc64d68319e4
Pavan Adithya <pavanadithyak.s@gmail.com>
2022-06-05 13:20:15 +0530

    dv

--- woof-woof/src/pages/Feed/FeedPage.js
+++ woof-woof/src/pages/Feed/FeedPage.js
@@ -6,13 +6,13 @@ import {GoLocation} from 'react-icons/go'
 import {MdOutlineContactPhone} from 'react-icons/md'
 import {TbVaccine} from 'react-icons/tb'
 
-const FeedPage = () => {
+const FeedPage = ({aname,alocation,aphone,avaccine,aimg}) => {
   return (
     <div className="feed">        
         <div className="feedWrapper">
             <div className='feedTop'>
                 <div className="feedTopLeft">      
-                    <span className="feedUserName">sample username</span>
+                    <span className="feedUserName">{aname}</span>
                     <span className="feedTime">5 mins ago</span>
                 </div>
                 <div className="feedTopRight">
@@ -22,58 +22,10 @@ const FeedPage = () => {
             <div className="feedCenter">
                 <img src='/uploads/2022-06-05T07-18-34.732ZAgriculture.jpg' alt='' className='feedImg'/><br/>
                 <div className='feedCenterBottom'>
-                    <span className="feedBreedName"><SiDatadog/><h5>Golden retriever</h5></span>
-                    <span className="feedLocation"><GoLocation/><h5>chennai</h5></span>
-                    <span className="feedContact"><MdOutlineContactPhone/><h5>9123456789</h5></span>
-                    <span className="feedVaccination"><TbVaccine/><h5>Not vaccinated</h5></span>     
-                </div> 
-            </div>
-            <div className="feedBottom">
-                <span className="feedLike"><FcLike/></span>
-                <span className="feedLikeCounter">20 People Liked it</span>
-            </div>
-        </div>
-        <div className="feedWrapper">
-            <div className='feedTop'>
-                <div className="feedTopLeft">      
-                    <span className="feedUserName">sample username</span>
-                    <span className="feedTime">5 mins ago</span>
-                </div>
-                <div className="feedTopRight">
-                    <FiMoreVertical/>
-                </div>
-            </div>
-            <div className="feedCenter">
-                <img src='/5.jpg' alt='' className='feedImg'/><br/>
-                <div className='feedCenterBottom'>
-                    <span className="feedBreedName"><SiDatadog/><h5>Golden retriever</h5></span>
-                    <span className="feedLocation"><GoLocation/><h5>chennai</h5></span>
-                    <span className="feedContact"><MdOutlineContactPhone/><h5>9123456789</h5></span>
-                    <span className="feedVaccination"><TbVaccine/><h5>Not vaccinated</h5></span>     
-                </div> 
-            </div>
-            <div className="feedBottom">
-                <span className="feedLike"><FcLike/></span>
-                <span className="feedLikeCounter">20 People Liked it</span>
-            </div>
-        </div>
-        <div className="feedWrapper">
-            <div className='feedTop'>
-                <div className="feedTopLeft">      
-                    <span className="feedUserName">sample username</span>
-                    <span className="feedTime">5 mins ago</span>
-                </div>
-                <div className="feedTopRight">
-                    <FiMoreVertical/>
-                </div>
-            </div>
-            <div className="feedCenter">
-                <img src='/5.jpg' alt='' className='feedImg'/><br/>
-                <div className='feedCenterBottom'>
-                    <span className="feedBreedName"><SiDatadog/><h5>Golden retriever</h5></span>
-                    <span className="feedLocation"><GoLocation/><h5>chennai</h5></span>
-                    <span className="feedContact"><MdOutlineContactPhone/><h5>9123456789</h5></span>
-                    <span className="feedVaccination"><TbVaccine/><h5>Not vaccinated</h5></span>     
+                    <span className="feedBreedName"><SiDatadog/><h5>{aname}</h5></span>
+                    <span className="feedLocation"><GoLocation/><h5>{alocation}</h5></span>
+                    <span className="feedContact"><MdOutlineContactPhone/><h5>{aphone}</h5></span>
+                    <span className="feedVaccination"><TbVaccine/><h5>{avaccine}</h5></span>     
                 </div> 
             </div>
             <div className="feedBottom">
